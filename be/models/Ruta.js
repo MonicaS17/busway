eaa07d810d4df5f1496e7c4ec5f273d174738679
Yaree29 <yarlep0220@gmail.com>
2026-06-14 18:39:27 -0500

    Creacion de models de viaje y ruta

---
+++ be/models/Ruta.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const RutaSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  escuela: { type: String, required: true },
+  conductor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+  zona: { type: String },
+  frecuencia: { type: String }
+}, { 
+  collection: 'rutas', 
+  timestamps: true 
+});
+
+module.exports = mongoose.models.Ruta || mongoose.model('Ruta', RutaSchema);
